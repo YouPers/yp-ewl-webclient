0579bb8ad680aef50b6f6e4e41de0251053bcedd
RBLU <reto.blunschi@creagy.ch>
2013-08-27 16:23:19 +0200

    translation of activity fields,

--- app/js/app.js
+++ app/js/app.js
@@ -3,10 +3,10 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('yp-ewl', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'globalErrors', 'ui.router', 'pascalprecht.translate']).
-    config(function ($stateProvider, $urlRouterProvider, $translateProvider) {
+    config(function ($stateProvider, $urlRouterProvider) {
         //
         // For any unmatched url, send to /home
-        $urlRouterProvider.otherwise("/home")
+        $urlRouterProvider.otherwise("/home");
         //
         // Now set up the states
         $stateProvider
@@ -86,7 +86,18 @@ angular.module('yp-ewl', ['myApp.filters', 'myApp.services', 'myApp.directives',
             HOW_PERCEIVE_CURRENT_LEVEL: 'How do you perceive your  stress level in this very moment?',
             I_AM_UNDER_CHALLENGED: 'I am bored and totally under challenged',
             I_AM_OVERLOADED: 'I am totally freaking out, cannot cope with everything',
-            AT_WORK: 'At work:'
+            AT_WORK: 'At work:',
+            WORK: 'Work',
+            LEISURE: 'Leisure',
+            TIME: 'Time',
+            NUTRITION: 'Nutrition',
+            EXERCISE: 'Exercise',
+            SOCIAL: 'Social',
+            BREAKS: 'Breaks',
+            COMMUTE: 'Commute',
+            PLAN_IT: 'plan it!',
+            PLANNED: 'planned',
+            SELECT_FIELD: 'Select an activity field:'
         });
 
         $translateProvider.preferredLanguage('de');
