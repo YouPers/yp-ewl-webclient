c9cfbcdf2fd470dba67a894112f07eb8010821f6
RBLU <reto.blunschi@creagy.ch>
2013-09-02 13:49:29 +0200

    add meaningful tests for activityController

--- test/unit/yp-common.spec.js
+++ test/unit/yp-common.spec.js
@@ -2,31 +2,38 @@
 
 /* jasmine specs for services go here */
 
-describe('i18n', function() {
-    var $scope = null;
-    var ctrl = null;
+describe('yp-common: ', function () {
 
-    beforeEach(module('i18n'));
+    describe('i18n: ', function () {
+        var $scope = null;
+        var ctrl = null;
 
-    /* IMPORTANT!
-     * this is where we're setting up the $scope and
-     * calling the controller function on it, injecting
-     * all the important bits, like our mockService */
-    beforeEach(inject(function($rootScope, $controller) {
+        beforeEach(module('i18n'));
 
-        //create a scope object for us to use.
-        $scope = $rootScope.$new();
+        /* IMPORTANT!
+         * this is where we're setting up the $scope and
+         * calling the controller function on it, injecting
+         * all the important bits, like our mockService */
+        beforeEach(inject(function ($rootScope, $controller) {
 
-        //now run that scope through the controller function,
-        //injecting any services or other injectables we need.
-        ctrl = $controller('i18nCtrl', {
-            $scope: $scope
+            //create a scope object for us to use.
+            $scope = $rootScope.$new();
+
+            //now run that scope through the controller function,
+            //injecting any services or other injectables we need.
+            ctrl = $controller('i18nCtrl', {
+                $scope: $scope
+            });
+        }));
+
+        describe('set language', function () {
+            it('should set correct language', inject(function ($translate) {
+                expect(ctrl).toBeDefined();
+                $scope.changeLang('DE');
+                expect($translate.uses()).toEqual('DE');
+                $scope.changeLang('EN');
+                expect($translate.uses()).toEqual('EN');
+            }));
         });
-    }));
-
-  describe('set language', function() {
-    it('should set correct language', inject(function($translate) {
-      expect(ctrl).toBeDefined();
-    }));
-  });
+    });
 });
