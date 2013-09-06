52b5379055e61f1f1578457f44cd669a38535a97
irig <9HHU1QYuT@gl>
2013-09-06 15:16:43 +0200

    Auslagerung Social Log Entries als Mock Data

--- app/js/ewl-sociallog.js
+++ app/js/ewl-sociallog.js
@@ -1,87 +1,74 @@
-function SocialLogCtrl($scope) {
-    $scope.socialLogEntries = [
-        {
-            "type": "community",
-            "status": "newMessage",
-            "avatar": "assets/img/UBAU.jpeg",
-            "author": "Urs Baumeler",
-            "area": "Bewegung",
-            "activity": "Joggen über Mittag",
-            "comment": "Bin auch dabei",
-            "timestamp": "20.08.2013 11:25"
-        },
-        {
-            "type": "community",
-            "status": "newMessage",
-            "avatar": "assets/img/IRIG.jpeg",
-            "author": "Ivan Rigamonti",
-            "area": "Bewegung",
-            "activity": "Joggen über Mittag",
-            "comment": "Kann leider nicht, bin ausser Haus.",
-            "timestamp": "20.08.2013 11:22"
-        },
-        {
-            "type": "youpers",
-            "status": "newMessage",
-            "avatar": "assets/img/YouPersAvatar.png",
-            "author": "YouPers",
-            "area": "Ernährung",
-            "activity": "Kohlenhydratarme Kost",
-            "comment": "Neue attraktive Ernährungsvorschläge!",
-            "timestamp": "20.08.2013 09:15"
-        },
-        {
-            "type": "community",
-            "status": "newMessage",
-            "avatar": "assets/img/RBLU.jpeg",
-            "author": "Reto Blunschi",
-            "area": "Bewegung",
-            "activity": "Joggen über Mittag",
-            "comment": "Welche Route heute?",
-            "timestamp": "20.08.2013 09:07"
-        },
-        {
-            "type": "community",
-            "status": "newMessage",
-            "avatar": "assets/img/SMUE.jpeg",
-            "author": "Stefan Müller",
-            "area": "Bewegung",
-            "activity": "Joggen über Mittag",
-            "comment": "Wer ist mit von der Partie?",
-            "timestamp": "20.08.2013 08:15"
-        },
-        {
-            "type": "community",
-            "status": "readMessage",
-            "avatar": "assets/img/UBAU.jpeg",
-            "author": "Urs Baumeler",
-            "area": "Bewegung",
-            "activity": "Joggen über Mittag",
-            "comment": "Kann dir nur zustimmen!",
-            "timestamp": "17.08.2013 16:35"
-        },
-        {
-            "type": "community",
-            "status": "readMessage",
-            "avatar": "assets/img/SMUE.jpeg",
-            "author": "Stefan Müller",
-            "area": "Bewegung",
-            "activity": "Joggen über Mittag",
-            "comment": "Wetter und Strecke waren einfach genial heute!",
-            "timestamp": "17.08.2013 14:22"
+'use strict';
+
+
+angular.module('yp.sociallog', []).
+
+    factory('SocialLogService', ['$http', function ($http) {
+
+        var socialLogService = {};
+
+        var socialLogEntries = $http.get('js/mockdata/testsociallog.json').then(function (result) {
+            return result.data;
+        })
+
+        socialLogService.getSocialLog = function () {
+            return socialLogEntries;
         }
-    ];
 
-    $scope.glyphicon = function(status) {
-        var icon = "";
-        if (status == "newMessage") {
-            icon = "envelope";
-        } else if (status == "readMessage") {
-            icon = "ok";
-        } else {
-            icon = "star";
-        };
-        return icon;
-    };
+        return socialLogService;
+    }
+    ])
 
-}
+    .controller('SocialLogCtrl', ['$scope', 'SocialLogService', function ($scope, SocialLogService) {
+
+        $scope.socialLogEntries = SocialLogService.getSocialLog();
+
+        $scope.getGlyphicon = function(status) {
+            var icon = "";
+            if (status == "newMessage") {
+                icon = "envelope";
+            } else if (status == "readMessage") {
+                icon = "ok";
+            } else {
+                icon = "star";
+            }
+            return icon;
+        }
+
+//        $scope.submitNewComment = function (thread) {
+//            var newComment = {
+//                thread_id: thread.id,
+//                author: {
+//                    link: 'bla',
+//                    fullname: 'Reto Blunschi',
+//                    pic: 'assets/pics/RBLU.jpeg'
+//                },
+//                date: new Date(),
+//                text: thread.newComment
+//            }
+//            thread.newComment=null;
+//            thread.comments.push(newComment);
+//        }
+
+//        $scope.submitNewThread = function (threads) {
+//            var newThread = {
+//                "ref_obj_id": 1,
+//                "ref_obj": "activity",
+//                "author": {
+//                    "id": 1,
+//                    "fullname": "Urs Baumeler",
+//                    "pic": "assets/img/UBAU.jpeg",
+//                    "link": "#/u/UBAU"
+//                },
+//                "date": new Date(),
+//                "type": "generic",
+//                "text": threads.newThread,
+//                "comments": []
+//            }
+//            threads.newThread = '';
+//            threads.showNewThread = false;
+//            threads.unshift(newThread);
+//        }
+
+    }])
+    
