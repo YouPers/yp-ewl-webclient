a5193752140d03389d622e2cc2552e068dba52ad
irig <9HHU1QYuT@gl>
2013-09-09 21:19:05 +0200

    Cockpit: Auswahl Zeitspanne der Balkendiagramme

--- app/js/ewl-app.js
+++ app/js/ewl-app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('yp-ewl', ['yp.ewl.assessment', 'yp.ewl.activity','yp.discussion','yp.sociallog', 'yp.actionlog', 'google-chart-sample', 'globalErrors', 'ui.router','ui.bootstrap',
+angular.module('yp-ewl', ['yp.ewl.assessment', 'yp.ewl.activity','yp.discussion','yp.sociallog', 'yp.actionlog', 'yp.ewl.cockpit-action-chart', 'globalErrors', 'ui.router','ui.bootstrap',
         'ngCookies', 'i18n', 'yp.filters', 'googlechart']).
     config(function ($stateProvider, $urlRouterProvider) {
         //
--- app/js/ewl-cockpit-action-chart.js
+++ app/js/ewl-cockpit-action-chart.js
@@ -1,12 +1,8 @@
 'use strict';
 
-angular.module('google-chart-sample', ['googlechart']).controller("CockpitActionChartCtrl", function ($scope) {
+angular.module('yp.ewl.cockpit-action-chart', ['googlechart']).controller("CockpitActionChartCtrl", function ($scope) {
 
-    var chart1 = {};
-    chart1.type = "ColumnChart";
-    chart1.displayed = false;
-    chart1.cssStyle = "height:250px; width:100%;";
-    chart1.data = {"cols": [
+    var dataCurrentWeek = {"cols": [
         {id: "actionCluster", label: "Aktivitätsbereich", type: "string"},
         {id: "done-id", label: "durchgeführt", type: "number"},
         {id: "missed-id", label: "verpasst", type: "number"}
@@ -40,6 +36,87 @@ angular.module('google-chart-sample', ['googlechart']).controller("CockpitAction
 
         ]}
     ]};
+
+    var dataCurrentMonth = {"cols": [
+        {id: "actionCluster", label: "Aktivitätsbereich", type: "string"},
+        {id: "done-id", label: "durchgeführt", type: "number"},
+        {id: "missed-id", label: "verpasst", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "Allgemein"},
+            {v: 18, f: "Alle geplanten Aktivitäten durchgeführt!"},
+            {v: 0}
+        ]},
+        {c: [
+            {v: "Fitness"},
+            {v: 50, f: "Vorbildiche Fitness!"},
+            {v: 5, f: "Möglicherweise wolltest du zuviel?"}
+        ]},
+        {c: [
+            {v: "Konsum"},
+            {v: 24, f: "Weiter so!"},
+            {v: 12, f: "Weniger ist oft mehr..."}
+
+        ]},
+        {c: [
+            {v: "Wohlbefinden"},
+            {v: 4},
+            {v: 8}
+
+        ]},
+        {c: [
+            {v: "Behandlungen"},
+            {v: 2},
+            {v: 0}
+
+        ]}
+    ]};
+
+    var dataCurrentYear = {"cols": [
+        {id: "actionCluster", label: "Aktivitätsbereich", type: "string"},
+        {id: "done-id", label: "durchgeführt", type: "number"},
+        {id: "missed-id", label: "verpasst", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "Allgemein"},
+            {v: 182, f: "Alle geplanten Aktivitäten durchgeführt!"},
+            {v: 22}
+        ]},
+        {c: [
+            {v: "Fitness"},
+            {v: 510, f: "Vorbildiche Fitness!"},
+            {v: 51, f: "Möglicherweise wolltest du zuviel?"}
+        ]},
+        {c: [
+            {v: "Konsum"},
+            {v: 96, f: "Weiter so!"},
+            {v: 22, f: "Weniger ist oft mehr..."}
+
+        ]},
+        {c: [
+            {v: "Wohlbefinden"},
+            {v: 16},
+            {v: 8}
+
+        ]},
+        {c: [
+            {v: "Behandlungen"},
+            {v: 12},
+            {v: 2}
+
+        ]}
+    ]};
+
+    var chart1 = {};
+
+    chart1.type = "ColumnChart";
+//    chart1.timespan = "Week";
+    chart1.displayed = false;
+    chart1.cssStyle = "height:250px; width:100%;";
+    chart1.data = dataCurrentWeek; // default: week
+    chart1.dataCurrentWeek = dataCurrentWeek;
+    chart1.dataCurrentMonth = dataCurrentMonth;
+    chart1.dataCurrentYear = dataCurrentYear;
 
     chart1.options = {
         "title": "Aktivitäten je Bereich",
