2a47930429d4ba9576a4384099a30d4bc0f9c9cc
RBLU <reto.blunschi@creagy.ch>
2013-09-25 22:12:20 +0200

    fixing heroku build

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,9 +73,9 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: process.env.PORT || 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'localhost'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
