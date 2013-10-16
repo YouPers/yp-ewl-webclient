f35dc7c70ddcfd77ea2a89fd198798fe52406d53
irig <9HHU1QYuT@gl>
2013-10-16 21:52:29 +0200

    Cockpit: Erste D3-Implementation (horizontal Bar)

--- app/js/d3-directives.js
+++ app/js/d3-directives.js
@@ -13,21 +13,38 @@
                 link: function(scope, iElement, iAttrs) {
                     d3Service.d3().then(function(d3){
 
-                        var canvasWidth = iAttrs.width;     // width as set in the <d3-bars width=...
-                        var canvasHeight = iAttrs.height;   // height as set in the <d3-bars height=...
+                        var barHeight = 30;         // height of a horizontal bar
+                        var barMargin = 5;          // margin between horizontal bars
+                        var xScaleTopHeight = 15;   // Height to store the top x scale
+                        var marginTop = 5;          // top margin
+                        var marginLeft = 5;         // left margin
+                        var marginBottom = 10;      // bottom margin
+                        var marginRight = 10;       // right margin
 
-                        var svg = d3.select(iElement[0])
-                                .append("svg")
-                                .attr("width", canvasWidth)
-                                .attr("height", canvasHeight)
-                                .attr("class", "youpers-chart")    // set class to link appropriate CSS
-                            .append("g")
-                            .attr("transform", "translate(5,5)")
-                            .attr("height", canvasHeight - 5)
-                            .attr("width", canvasWidth - 5);
+                        var actualHeightCompressed = (scope.data.length * (barHeight + barMargin)) - barMargin + xScaleTopHeight + marginBottom;
 
-//                        ;
+                        // svgCanvas: Container with n g-containers
+                        // only chartCanvas is stored in this container
+
+                        var svgCanvas = d3.select(iElement[0])
+                            .append("svg")
+                            .attr("width", iAttrs.width)        // width as set in the <d3-bars width=...
+                            .attr("height", iAttrs.height)      // height as set in the <d3-bars height=...
+                            .attr("class", "youpers-chart");    // set class to link appropriate CSS
+
+                        if (iAttrs.d3HeightCompress === "yes") {  //
+                            svgCanvas.attr('height', actualHeightCompressed);
+                        }
+
+                        // chartCanvas: the only element stored in svgCanvas
+
+                        var chartCanvas = svgCanvas.append("g")
+                            .attr("transform", "translate(" + marginTop + "," + marginLeft + ")")    // top/left margin to avoid cropped elements
+                            .attr("height", "100%")                 // fills the complete height of the parent container
+                            .attr("width", "100%");                 // fills the complete width of the parent container
+
                         // on window resize, re-render d3 canvas
+
                         window.onresize = function() {
                             return scope.$apply();
                         };
@@ -58,38 +75,27 @@
                             }
 
                             // remove all previous items before render
-                            svg.selectAll("*").remove();
+
+                            chartCanvas.selectAll("*").remove();
 
                             // setup variables
-                            var currentWidth,       // current width in px taking resizing into account
-                                allBarsHeight,      // height of all bars, to be used if <d3-bars height="0"
-                                actualHeight,       // height used for canvas
-                                maxValue,           // highest value of data array, needed to scale appropriately
-                                marginLeft,         // margin left of the bars to be used
-                                marginRight,        // margin right of the bars to be used
-                                marginTop,          // margin on top of the bars to be used
-                                marginBottom,       // margin on bottom of the bars to be used
-                                barHeight,          // height of a horizontal bar
-                                barMargin;    // margin between horizontal bars
 
-                            marginLeft = 10;
-                            marginRight = 10;
-                            marginTop = 10;
-                            marginBottom = 10;
-                            barHeight = 30;
-                            barMargin = 5;
+                            var currentWidth,       // current width in px taking resizing into account
+                                actualHeightFinal;  // height if d3-height-compress="no"
 
                             currentWidth = d3.select(iElement[0])[0][0].offsetWidth - (marginLeft + marginRight);
-                            allBarsHeight = (scope.data.length * (barHeight + barMargin)) + barMargin;
-                            maxValue = d3.max(data, function (d) { return d.value;});
+                            actualHeightCompressed = (scope.data.length * (barHeight + barMargin)) - barMargin;
+//                            maxValue = d3.max(data, function (d) { return d.value;});
 
                             // set the height
 
-                            if (svg.attr("height") === "0") {
-                                actualHeight = allBarsHeight;
-                                svg.attr('height', actualHeight);
+                            if (iAttrs.d3HeightCompress === "yes") {
+                                actualHeightFinal = actualHeightCompressed;
+                                chartCanvas.attr('height', actualHeightCompressed);
                             } else {
-                                actualHeight = svg.attr("height");
+                                actualHeightFinal = iAttrs.height - marginTop - xScaleTopHeight - (marginBottom / 2);
+                                barHeight = (barHeight * iAttrs.height / actualHeightCompressed);
+                                barHeight = ((actualHeightFinal + barMargin) / scope.data.length) - barMargin;
                             }
 
                             // the x and y scales
@@ -99,40 +105,49 @@
                                 .range([0, currentWidth]);
 
                             var y = d3.scale.ordinal()
-//                                .domain(values)
                                 .domain(d3.range(scope.data.length))
-                                .rangeRoundBands([0, actualHeight], (barMargin /barHeight), (marginTop /barHeight));
-//                                .rangeBands([0, actualHeight]);
+                                .rangeRoundBands([0, actualHeightFinal], (barMargin /barHeight), (marginTop /barHeight));
+//                                .rangeBands([0, actualHeightFinal]);
 
-                            //create the rectangles for the bar chart
+                            // Canvas for the top x scale
 
-                            var g = svg.append("g")
+                            var xScaleTopCanvas = chartCanvas.append("g")      //
+                                .attr("height", xScaleTopHeight)
+                                .attr("width", "100%");
+
+                            // Canvas for the all elements on the chart grid
+
+                            var gridCanvas = chartCanvas.append("g")
                                 .attr("transform", "translate(0,15)")
-                                .attr("height", canvasHeight - 15)
-                                .attr("width", canvasWidth);
+                                .attr("height", "100%")
+                                .attr("width", "100%");
 
-                            g.selectAll("line")
-                                .data(x.ticks(10))
-                                .enter().append("line")
-                                .attr("x1", x)
-                                .attr("x2", x)
-                                .attr("y1", 0)
-                                .attr("y2", actualHeight)
-//                                .attr("dx", marginLeft + 50)
-                                .style("stroke", "#ccc");
+                            // draw the x scale values
 
-                            svg.selectAll(".rule")
+                            xScaleTopCanvas.selectAll(".rule")
                                 .data(x.ticks(10))
                                 .enter().append("text")
                                 .attr("class", "rule")
                                 .attr("x", x)
                                 .attr("y", 0)
-                                .attr("dy", marginTop)
+                                .attr("dy", "0.75em")
                                 .attr("text-anchor", "middle")
-//                                .attr("dx", marginLeft + 5)
                                 .text(String);
 
-                            g.selectAll("rect")
+                            // draw the x scale ticks
+
+                            gridCanvas.selectAll("line")      //
+                                .data(x.ticks(10))
+                                .enter().append("line")
+                                .attr("x1", x)
+                                .attr("x2", x)
+                                .attr("y1", 0)
+                                .attr("y2", actualHeightFinal)
+                                .style("stroke", "#ccc");
+
+                            //create the rectangles for the bar chart
+
+                            gridCanvas.selectAll("rect")
                                 .data(values)
                                 .enter()
                                 .append("rect")
@@ -144,25 +159,23 @@
                                 })
                                 .attr("height", barHeight)  // height of each bar
                                 .attr("width", 0)           // initial width of 0 for transition
-//                                .attr("x", x)
                                 .attr("x", 0)
-//                                .attr("y", y) // does not work!!
                                 .attr('y', function(d,i) {
-                                    return i * (barHeight + marginTop);
+                                    return i * (barHeight + barMargin);
                                 })
                                 .transition()
-                                .duration(2000)             // time of duration
+                                .duration(1000)             // time of duration
                                 .attr("width", function(d) {
                                     return x(d);
                                 })
                             ;
 
-                            g.selectAll("value")
+                            gridCanvas.selectAll("value")
                                 .data(values)
                                 .enter()
                                 .append("text")
                                 .attr("y", function(d,i) {
-                                    return i * (barHeight + marginTop);
+                                    return i * (barHeight + barMargin);
                                 })
                                 .attr("dy", "1.35em")
                                 .attr("dx", -5)
@@ -175,14 +188,14 @@
                                     return d.toString();
                                 });
 
-                            g.selectAll("label")
+                            gridCanvas.selectAll("label")
                                 .data(labels)
                                 .enter()
                                 .append("text")
                                 .attr("dy", "1.35em")
-                                .attr("y", y) // does not work!!
+                                .attr("y", y)   // does not work, that's why I overwrite it!!
                                 .attr("y", function(d,i) {
-                                    return i * (barHeight + marginTop);
+                                    return i * (barHeight + barMargin);
                                 })
                                 .attr("x", 0)
                                 .attr("dx", 5)
@@ -191,9 +204,9 @@
                                     return d;
                                 });
 
-                            g.append("line")
+                            gridCanvas.append("line")
                                 .attr("y1", 0)
-                                .attr("y2", actualHeight)
+                                .attr("y2", actualHeightFinal)
                                 .style("stroke", "#000");
 
                         };
