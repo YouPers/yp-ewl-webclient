85d8c6be0fd10baf26334f9a95e5426a2e4aed92
irig <9HHU1QYuT@gl>
2013-10-22 20:12:48 +0200

    Cockpit: - horizontal bar chart: using same data as vertical Google chart - d3-directive: fixed in order to handle new data

--- app/js/yp-ewl-activity-chart2.js
+++ app/js/yp-ewl-activity-chart2.js
@@ -4,19 +4,30 @@ angular.module('yp.ewl.activity.chart2', [])
 
     .factory('yp.ewl.activity.chart2.service', ['$http', function($http) {
 
+        var convertDataSeries = function (data) {
+            var cols = data.cols;
+            var rows = data.rows;
+            var chartData = {};
+            chartData.series = [];
+            for (var i = 0; i < rows.length; i++) {
+                chartData.series.push( {label: rows[i].c[0].v, value: rows[i].c[1].v});
+            }
+            return chartData;
+        };
+
         var valuesThisWeek = $http.get('activitystats?range=weekly')
             .then(function (result) {
-                return result.data;
+                return convertDataSeries(result.data);
             });
 
         var valuesThisMonth = $http.get('activitystats?range=monthly')
             .then(function (result) {
-                return result.data;
+                return convertDataSeries(result.data);
             });
 
         var valuesThisYear = $http.get('activitystats?range=yearly')
             .then(function (result) {
-                return result.data;
+                return convertDataSeries(result.data);
             });
 
         var ActivityChartService = {
@@ -30,14 +41,6 @@ angular.module('yp.ewl.activity.chart2', [])
     }])
 
     .controller('yp.ewl.activity.chart2.controller', ['$scope', 'yp.ewl.activity.chart2.service', function ($scope, ActivityChartService) {
-
-        $scope.d3Data = [
-            {label: "Allgemein", value: 122},
-            {label: "Fitness", value: 205},
-            {label: 'Konsum', value: 96},
-            {label: "Wohlbefinden", value: 66},
-            {label: "Behandlungen", value: 42}
-        ];
 
         $scope.d3Options = {
 //            compressed: "no",
