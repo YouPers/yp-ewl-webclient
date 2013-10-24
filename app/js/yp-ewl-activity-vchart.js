3bfad463e632d26be08013850b36fd320aedafe6
irig <9HHU1QYuT@gl>
2013-10-24 19:43:46 +0200

    Cockpit: D3 vertical bar copy from horizontal bar as a starting base

---
+++ app/js/yp-ewl-activity-vchart.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('yp.ewl.activity.vchart', [])
+
+    .factory('yp.ewl.activity.vchart.service', ['$http', function($http) {
+
+        var convertDataSeries = function (data) {
+//            var cols = data.cols;
+            var rows = data.rows;
+            var chartData = {};
+            chartData.series = [];
+            for (var i = 0; i < rows.length; i++) {
+                chartData.series.push( {label: rows[i].c[0].v, value: rows[i].c[1].v});
+            }
+            return chartData;
+        };
+
+        var valuesThisWeek = $http.get('activitystats?range=weekly')
+            .then(function (result) {
+                return convertDataSeries(result.data);
+            });
+
+        var valuesThisMonth = $http.get('activitystats?range=monthly')
+            .then(function (result) {
+                return convertDataSeries(result.data);
+            });
+
+        var valuesThisYear = $http.get('activitystats?range=yearly')
+            .then(function (result) {
+                return convertDataSeries(result.data);
+            });
+
+        var ActivityChartService = {
+            activitiesThisWeek: valuesThisWeek,
+            activitiesThisMonth: valuesThisMonth,
+            activitiesThisYear: valuesThisYear
+        };
+
+        return ActivityChartService;
+
+    }])
+
+    .controller('yp.ewl.activity.vchart.controller', ['$scope', 'yp.ewl.activity.vchart.service', function ($scope, ActivityChartService) {
+
+        $scope.d3Options = {
+//            compressed: "no",
+//            chartHeight: "250",
+            chartWidth: "99%"
+        };
+
+        $scope.chart = {};
+
+        ActivityChartService.activitiesThisWeek.then(function (data) {
+            $scope.chart.dataCurrentWeek = data;
+            $scope.chart.data = data; // default value
+        });
+
+        ActivityChartService.activitiesThisMonth.then(function (data) {
+            $scope.chart.dataCurrentMonth = data;
+        });
+
+        ActivityChartService.activitiesThisYear.then(function (data) {
+            $scope.chart.dataCurrentYear = data;
+        });
+
+    }]);
