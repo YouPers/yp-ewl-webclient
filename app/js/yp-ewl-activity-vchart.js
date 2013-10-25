3ac1a92e55cb9c8b615bf30e66ed82cc69766389
irig <9HHU1QYuT@gl>
2013-10-25 15:55:18 +0200

    Cockpit: D3 vertical BarChart and Google Charts removed

--- app/js/yp-ewl-activity-vchart.js
+++ app/js/yp-ewl-activity-vchart.js
@@ -4,16 +4,16 @@ angular.module('yp.ewl.activity.vchart', ['restangular'])
 
     .factory('yp.ewl.activity.vchart.service', ['Restangular', function(Restangular) {
 
-        var convertDataSeries = function (data) {
+//        var convertDataSeries = function (data) {
 //            var cols = data.cols;
-            var rows = data.rows;
-            var chartData = {};
-            chartData.series = [];
-            for (var i = 0; i < rows.length; i++) {
-                chartData.series.push( {label: rows[i].c[0].v, value: rows[i].c[1].v});
-            }
-            return chartData;
-        };
+//            var rows = data.rows;
+//            var chartData = {};
+//            chartData.series = [];
+//            for (var i = 0; i < rows.length; i++) {
+//                chartData.series.push( {label: rows[i].c[0].v, value: rows[i].c[1].v});
+//            }
+//            return chartData;
+//        };
 
         var valuesThisWeek = Restangular.one('activitystats?range=weekly').get()
             .then(function (result) {
