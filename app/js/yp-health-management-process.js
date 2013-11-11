6cfc03ef911a2021607c189eb5f411f1cd18b1c2
irig <9HHU1QYuT@gl>
2013-11-11 22:50:28 +0100

    Health Management Process: Process Navigation with d3/SVG fixing digest loop

--- app/js/yp-health-management-process.js
+++ app/js/yp-health-management-process.js
@@ -18,6 +18,26 @@ angular.module('yp.health-management-process', ['restangular'])
 
     .controller('yp.health-management-process.controller', ['$scope', 'yp.health-management-process.service', function ($scope, HealthManagementProcessService) {
 
+        $scope.d3OptionsCommit = {
+            displayType: "commit"
+        };
+
+        $scope.d3OptionsAssess = {
+            displayType: "assess"
+        };
+
+        $scope.d3OptionsPlan = {
+            displayType: "plan"
+        };
+
+        $scope.d3OptionsDo = {
+            displayType: "do"
+        };
+
+        $scope.d3OptionsEvaluate = {
+            displayType: "evaluate"
+        };
+
         $scope.processData = HealthManagementProcessService;
 
     }]);
