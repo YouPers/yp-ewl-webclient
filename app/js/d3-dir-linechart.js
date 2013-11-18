2157d93e78a022afec0cbca14cd3ef154dfe2855
irig <9HHU1QYuT@gl>
2013-11-18 22:28:55 +0100

    Cockpit: General stress level line chart now with date x scale and gradient line

--- app/js/d3-dir-linechart.js
+++ app/js/d3-dir-linechart.js
@@ -46,7 +46,6 @@
 
                     function draw (d3, data) {
 
-//                        var series = data.slice(0,10);
                         var series = data;
 
                         var initParameters = function () {
@@ -66,6 +65,11 @@
                             // - ideally set as %-value to elastically fit into the parent container of the chart
 
                             scope.options.chartWidth = scope.options.chartWidth || "100%";
+
+                            // scope.options.xScaleTopHeight
+                            // height of the space containing the x scale on top of the chart
+
+                            scope.options.yScaleLeftWidth = scope.options.yScaleLeftWidth || 85;
 
                             // scope.options.xScaleTopHeight
                             // height of the space containing the x scale on top of the chart
@@ -135,7 +139,7 @@
                             var currentWidth,       // current width in px taking resizing into account
                                 actualHeightFinal;  // height if d3-height-compress="no"
 
-                            currentWidth = d3.select(iElement[0])[0][0].offsetWidth - (scope.options.marginLeft + scope.options.marginRight);
+                            currentWidth = d3.select(iElement[0])[0][0].offsetWidth - (scope.options.marginLeft + scope.options.marginRight) - scope.options.yScaleLeftWidth;
 
                             // set the height
 
@@ -143,8 +147,18 @@
 
                             // the x and y scales
 
-                            var x = d3.scale.linear()
-                                .domain([0, series.length])
+//                            var x = d3.scale.linear()
+//                                .domain([0, series.length])
+//                                .range([0, currentWidth]);
+
+//                            var parseDate = d3.time.format("%d.%m.%y");
+//
+//                            angular.forEach(series, function (entry) {
+//                               entry.date = parseDate(entry.date);
+//                            });
+
+                            var x = d3.time.scale()
+                                .domain([series[0].date, d3.time.day.offset(series[series.length - 1].date, 1)])
                                 .range([0, currentWidth]);
 
                             var y = d3.scale.linear()
@@ -154,27 +168,53 @@
                             // Canvas for the top x scale
 
                             var xScaleTopCanvas = chartCanvas.append("g")
+                                .attr("transform", "translate(" + scope.options.yScaleLeftWidth + ",0)")
                                 .attr("height", scope.options.xScaleTopHeight)
                                 .attr("width", "100%");
+
+                            var yScaleLeftCanvas = chartCanvas.append("g")
+                                .attr("height", "100%")
+                                .attr("width", scope.options.yScaleLeftWidth);
+
+                            yScaleLeftCanvas.append("text")
+                                .attr("x", 0)
+                                .attr("y", scope.options.marginTop + scope.options.xScaleTopHeight)
+                                .text(scope.options.chartMaxText);
+
+                            yScaleLeftCanvas.append("text")
+                                .attr("x", 0)
+                                .attr("y", scope.options.chartHeight / 2)
+                                .text(scope.options.chartMidText);
+
+                            yScaleLeftCanvas.append("text")
+                                .attr("x", 0)
+                                .attr("y", scope.options.chartHeight - scope.options.marginBottom)
+                                .text(scope.options.chartMinText);
+
+//                            scope.options.yScaleLeftWidth
 
                             // Canvas for the all elements on the chart grid
 
                             var gridCanvas = chartCanvas.append("g")
-                                .attr("transform", "translate(0,15)")
+//                                .attr("transform", "translate(0,15)")
+                                .attr("transform", "translate(" + scope.options.yScaleLeftWidth + ",15)")
                                 .attr("height", "100%")
                                 .attr("width", "100%");
 
                             // draw the x scale values
 
                             xScaleTopCanvas.selectAll(".rule")
-                                .data(x.ticks(10))
+                                .data(x.ticks(5))
                                 .enter().append("text")
                                 .attr("class", "rule")
                                 .attr("x", x)
                                 .attr("y", 0)
                                 .attr("dy", "0.75em")
                                 .attr("text-anchor", "middle")
-                                .text(String);
+                                .text(function (d) {
+                                    return d.getDate() + "." +  (d.getMonth()+1) + ".";
+//                                    return String;
+                                });
 
                             // draw the x scale ticks
 
@@ -191,27 +231,48 @@
                                 .data(y.ticks(3))
                                 .enter().append("line")
                                 .attr("x1", 0)
-                                .attr("x2", scope.options.chartWidth)
+                                .attr("x2", currentWidth)
                                 .attr("y1", y)
                                 .attr("y2", y)
                                 .style("stroke", "#ccc");
 
+                            gridCanvas.append("linearGradient")
+                                .attr("id", "line-gradient")
+                                .attr("gradientUnits", "userSpaceOnUse")
+                                .attr("x1", 0).attr("y1", y(-100))
+                                .attr("x2", 0).attr("y2", y(100))
+                                .selectAll("stop")
+                                .data([
+                                    {offset: "0%", color: "#DC3912"},
+                                    {offset: "12%", color: "#DC3912"},
+                                    {offset: "18%", color: "#FF9900"},
+                                    {offset: "33%", color: "#FF9900"},
+                                    {offset: "43%", color: "#109618"},
+                                    {offset: "57%", color: "#109618"},
+                                    {offset: "67%", color: "#FF9900"},
+                                    {offset: "82%", color: "#FF9900"},
+                                    {offset: "88%", color: "#DC3912"},
+                                    {offset: "100%", color: "#DC3912"}
+                                ])
+                                .enter().append("stop")
+                                .attr("offset", function(d) { return d.offset; })
+                                .attr("stop-color", function(d) { return d.color; });
+
                             var line = d3.svg.line()
                                 .x(function(d,i) {
-                                    return x(i);
+                                    return x(d.date);
+//                                    return x(i);
                                 })
                                 .y(function(d) {
-                                    return y(d);
+                                    return y(d.value);
                                 });
 
-                            //create the rectangles for the bar chart
+                            //create the line
 
                             gridCanvas
                                 .append("svg:path")
                                 .attr("d", line(series))
-                                .style("stroke", "steelblue")
-                                .style("stroke-width", "2")
-                                .style("fill", "none");
+                                .attr("class", "youpers-stress-level-line");
 
                         };
 
--- app/js/yp-ewl-stresslevel-line-chart.js
+++ app/js/yp-ewl-stresslevel-line-chart.js
@@ -6,13 +6,32 @@ angular.module('yp.ewl.stresslevel.linechart', ['yp.ewl.assessment'])
 
         AssessmentService.getAssessmentResults('525faf0ac558d40000000005', 'timestamp:1')
             .then(function (result) {
+                $scope.generalStressLevelTitle = "";
+                $scope.generalStressLevelMinText = "";
+                $scope.generalStressLevelMidText = "";
+                $scope.generalStressLevelMaxText = "";
                 $scope.generalStressLevelValues = [];
                 if (result) {
                     if (result.length > 0) {
                         angular.forEach(result, function (resultEntry) {
+                            var assessmentDate = new Date(resultEntry.timestamp);
                             angular.forEach(resultEntry.answers, function (question) {
                                 if (question.question === "5278c51a6166f2de240000df") {
-                                    $scope.generalStressLevelValues.push(question.answer);
+                                    if ($scope.generalStressLevelTitle.length === 0) {
+                                    AssessmentService.getAssessment('525faf0ac558d40000000005')
+                                        .then(function (result) {
+                                            if (result) {
+                                                $scope.generalStressLevelTitle = result.questionLookup[question.question].title;
+                                                $scope.generalStressLevelMinText = result.questionLookup[question.question].mintext;
+                                                $scope.generalStressLevelMidText = result.questionLookup[question.question].midtext;
+                                                $scope.generalStressLevelMaxText = result.questionLookup[question.question].maxtext;
+                                            }
+                                        });
+                                    }
+                                    $scope.generalStressLevelValues.push(
+                                        { "date": assessmentDate,
+                                            "value" : question.answer,
+                                        "title": $scope.generalStressLevelTitle});
                                 }
                             });
                         });
@@ -20,10 +39,24 @@ angular.module('yp.ewl.stresslevel.linechart', ['yp.ewl.assessment'])
                 }
             });
 
+        AssessmentService.getAssessment('525faf0ac558d40000000005')
+            .then(function (result) {
+                if (result) {
+                    $scope.generalStressLevelTitle = result.questionLookup["5278c51a6166f2de240000df"].title;
+                    $scope.generalStressLevelMinText = result.questionLookup["5278c51a6166f2de240000df"].mintext;
+                    $scope.generalStressLevelMidText = result.questionLookup["5278c51a6166f2de240000df"].midtext;
+                    $scope.generalStressLevelMaxText = result.questionLookup["5278c51a6166f2de240000df"].maxtext;
+                }
+                $scope.d3Options = {
+                    chartTitle:  $scope.generalStressLevelTitle,
+                    chartMinText:  $scope.generalStressLevelMinText,
+                    chartMidText:  $scope.generalStressLevelMidText,
+                    chartMaxText:  $scope.generalStressLevelMaxText,
+                    chartHeight: 127
+                };
+            });
 
-        $scope.d3Options = {
-            chartHeight: 127
-        };
+
 
 
     }]);
