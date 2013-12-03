12a06c30b7af754557bf9d1db490db093f7af666
ragnox <boris.wechselberger@gmail.com>
2013-12-03 22:12:39 +0100

    refactor header to directive, d3 graphics to css

---
+++ app/js/directives/header.js
@@ -0,0 +1,22 @@
+angular.module('yp.commons', [])
+    .directive('header', function () {
+        return {
+            restrict: 'EA',
+            templateUrl: 'js/directives/header.html',
+            scope: { }, // isolated
+            link: function (scope, elem, attrs) {
+
+                scope.items = [
+                    { "name": "commit", "url": "#/topics"},
+                    { "name": "assess", "url": "#/assessment/525faf0ac558d40000000005"},
+                    { "name": "plan", "url": "#/activities"},
+                    { "name": "do", "url": "#/cockpit"},
+                    { "name": "evaluate", "url": "#/evaluate"}
+                ];
+
+                scope.isActive = function(index) {
+                    return attrs.name === scope.items[index].name;
+                };
+            }
+        }
+    });
