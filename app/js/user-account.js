96bf6bcb44f1ad2db7a7fc48807dbd5259ccbb76
RBLU <reto.blunschi@youpers.com>
2014-01-16 12:54:51 +0100

    pull request comments

--- app/js/user-account.js
+++ app/js/user-account.js
@@ -21,30 +21,24 @@
                         templateUrl: "partials/email.verification.html",
                         controller: "EmailVerificationCtrl",
                         access: accessLevels.individual,
-                        resolve: {
-
-                        }
+                        resolve: { }
                     })
 
-                .state('requestPasswordReset', {
-                    url: "/requestPasswordReset",
-                    templateUrl: "partials/user.requestpasswordreset.html",
-                    controller: "RequestPasswordResetCtrl",
-                    access: accessLevels.anonymous,
-                    resolve: {
+                    .state('requestPasswordReset', {
+                        url: "/requestPasswordReset",
+                        templateUrl: "partials/user.requestpasswordreset.html",
+                        controller: "RequestPasswordResetCtrl",
+                        access: accessLevels.anonymous,
+                        resolve: { }
+                    })
 
-                    }
-                })
-
-                .state('passwordReset', {
-                    url: "/password_reset/:token?firstname&lastname",
-                    templateUrl: "partials/user.passwordreset.html",
-                    controller: "PasswordResetCtrl",
-                    access: accessLevels.anonymous,
-                    resolve: {
-
-                    }
-                });
+                    .state('passwordReset', {
+                        url: "/password_reset/:token?firstname&lastname",
+                        templateUrl: "partials/user.passwordreset.html",
+                        controller: "PasswordResetCtrl",
+                        access: accessLevels.anonymous,
+                        resolve: { }
+                    });
             }])
 
 
@@ -101,45 +95,45 @@
 
             }])
 
-    .controller('PasswordResetCtrl', ['$scope', '$state','$rootScope', 'authority', 'yp.user.UserService',
-        function ($scope, $state, $rootScope, authority, UserService) {
+        .controller('PasswordResetCtrl', ['$scope', '$state', '$rootScope', 'authority', 'yp.user.UserService',
+            function ($scope, $state, $rootScope, authority, UserService) {
 
-            $scope.firstname = $state.params.firstname;
-            $scope.lastname = $state.params.lastname;
+                $scope.firstname = $state.params.firstname;
+                $scope.lastname = $state.params.lastname;
 
-            $scope.passwordResetObjReset = function () {
-                $scope.passwordResetObj = {password: '', password2: ''};
-                $scope.$broadcast('formPristine');
-            };
-            $scope.passwordResetObjReset();
+                $scope.passwordResetObjReset = function () {
+                    $scope.passwordResetObj = {password: '', password2: ''};
+                    $scope.$broadcast('formPristine');
+                };
+                $scope.passwordResetObjReset();
 
-            $scope.passwordReset = function () {
-                UserService.passwordReset($state.params.token, $scope.passwordResetObj.password ).then(function () {
-                    $scope.resetSuccessful = true;
-                }, function (err) {
-                    $rootScope.$broadcast('globalUserMsg', "Passwort konnte nicht gesetzt werden.", 'danger', 3000);
-                });
-            };
+                $scope.passwordReset = function () {
+                    UserService.passwordReset($state.params.token, $scope.passwordResetObj.password).then(function () {
+                        $scope.resetSuccessful = true;
+                    }, function (err) {
+                        $rootScope.$broadcast('globalUserMsg', "Passwort konnte nicht gesetzt werden.", 'danger', 3000);
+                    });
+                };
 
-        }])
+            }])
 
-    .controller('RequestPasswordResetCtrl', ['$scope', '$rootScope', 'authority', 'yp.user.UserService',
-        function ($scope, $rootScope, authority, UserService) {
+        .controller('RequestPasswordResetCtrl', ['$scope', '$rootScope', 'authority', 'yp.user.UserService',
+            function ($scope, $rootScope, authority, UserService) {
 
 
-            $scope.passwordResetObjReset = function () {
-                $scope.passwordResetObj = {usernameOrEmail: ''};
-            };
-            $scope.passwordResetObjReset();
+                $scope.passwordResetObjReset = function () {
+                    $scope.passwordResetObj = {usernameOrEmail: ''};
+                };
+                $scope.passwordResetObjReset();
 
-            $scope.requestPasswordReset = function () {
-                UserService.requestPasswordReset($scope.passwordResetObj.usernameOrEmail).then(function () {
-                    $scope.requestSent = true;
-                }, function (err) {
-                    $rootScope.$broadcast('globalUserMsg', "Dieser Username/Email ist uns nicht bekannt", 'danger', 3000);
-                });
-            };
+                $scope.requestPasswordReset = function () {
+                    UserService.requestPasswordReset($scope.passwordResetObj.usernameOrEmail).then(function () {
+                        $scope.requestSent = true;
+                    }, function (err) {
+                        $rootScope.$broadcast('globalUserMsg', "Dieser Username/Email ist uns nicht bekannt", 'danger', 3000);
+                    });
+                };
 
-        }]);
+            }]);
 
-        }());
+}());
