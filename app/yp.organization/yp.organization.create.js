d9db22b4508987578d24090b7d92f75b80065ae6
Boris <boris.wechselberger@youpers.com>
2014-02-04 14:08:44 +0100

    WL-56 / WL-227 organization view

---
+++ app/yp.organization/yp.organization.create.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+
+    angular.module('yp.organization')
+
+
+        .controller('CreateOrganizationController', ['$scope', 'OrganizationService',
+            function ($scope, OrganizationService) {
+
+                var getOrganizations = function() {
+                    OrganizationService.getOrganizations().then(function(organizations) {
+                        $scope.organization = organizations[0];
+                    });
+                };
+                getOrganizations();
+
+
+                $scope.organizationObj = {};
+
+                $scope.createOrganization = function() {
+                    OrganizationService.postOrganization($scope.organizationObj, function(organization) {
+                        $scope.organization = organization;
+                    });
+                };
+
+            }]);
+}());
