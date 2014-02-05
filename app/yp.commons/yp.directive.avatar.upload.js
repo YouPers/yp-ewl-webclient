1ac380d6c38cd3650d475b54aefd6751b6b8c0d4
Boris <boris.wechselberger@youpers.com>
2014-02-05 13:13:00 +0100

    WL-56 / WL-239 avatar directive, organization avatar

---
+++ app/yp.commons/yp.directive.avatar.upload.js
@@ -0,0 +1,103 @@
+(function () {
+    'use strict';
+
+    angular.module('yp.commons')
+
+
+        /**
+         * directive: avatar-upload
+         *
+         * dependencies: avatarObject in parent scope, with the avatar at avatarObject.avatar
+         *
+         * default: user avatar for the authenticated user
+         *
+         * optional attributes:
+         *
+         *  - type: [ 'organization' ]
+         *  - objectId: the object id for the specified model type
+         *
+         */
+        .directive('avatarUpload', ['$rootScope', '$http', '$fileUploader', 'yp.config',
+            function ($rootScope, $http, $fileUploader, config) {
+                return {
+                    restrict: 'E',
+                    templateUrl: 'yp.commons/yp.directive.avatar.upload.html',
+                    link: function (scope, elem, attrs) {
+
+                        var uploader;
+
+                        if(!scope.avatarObject && (!attrs.avatarObject || !scope[attrs.avatarObject])) {
+                            throw "avatar-upload: avatarObject not found or specified"
+                        } else {
+                            var avatarObject = scope.avatarObject ? 'avatarObject' : attrs.avatarObject;
+                            scope.$watch(avatarObject, function(avatarObject) {
+
+                                if(!avatarObject || _.isEmpty(avatarObject)) {
+                                    return;
+                                }
+
+                                scope.avatarObject = avatarObject;
+
+                                var url;
+
+                                if(attrs.type && attrs.type === 'organization') {
+                                    url = config.backendUrl + "/organizations/" + scope.avatarObject.id + "/avatar";
+                                } else {
+                                    url = config.backendUrl + "/users/" + scope.avatarObject.id + "/avatar";
+                                }
+
+                                uploader = scope.uploader = $fileUploader.create({
+                                    scope: scope,
+                                    url: url,
+                                    autoUpload: true,
+                                    headers: {
+                                        'Authorization': $http.defaults.headers.common.Authorization
+                                    }
+                                });
+
+
+                                // images only filter
+                                uploader.filters.push(function(item /*{File|HTMLInputElement}*/) {
+                                    var type = uploader.isHTML5 ? item.type : '/' + item.value.slice(item.value.lastIndexOf('.') + 1);
+                                    type = '|' + type.toLowerCase().slice(type.lastIndexOf('/') + 1) + '|';
+                                    var valid = '|jpg|png|jpeg|bmp|gif|tif|tiff'.indexOf(type) !== -1;
+                                    if(!valid) {
+                                        scope.$apply(function() {
+                                            $rootScope.$broadcast('globalUserMsg', "invalid image file", 'danger', 3000);
+
+                                        });
+                                    }
+                                    return  valid;
+                                });
+
+
+                                // on file upload complete
+                                uploader.bind('error', function (event, xhr, item, response) {
+                                    scope.$apply(function() {
+                                        $rootScope.$broadcast('globalUserMsg', "error while processing image file", 'danger', 3000);
+
+                                    });
+                                });
+                                // on file upload complete
+                                uploader.bind('success', function (event, xhr, item, response) {
+                                    if(scope.avatarObject) {
+
+                                        scope.avatarObject.avatar = response.avatar;
+                                    }
+                                });
+                            });
+                        }
+                    }
+                };
+        }])
+
+
+        .directive('avatar', function() {
+            return function(scope, element, attrs) {
+
+                scope.$watch('avatarObject.avatar', function(avatar) {
+                    element.css("background-image", "url(" + avatar + ")");
+                });
+            };
+        });
+}());
