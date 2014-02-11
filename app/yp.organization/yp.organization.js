701acbae75704d4da2d11b432a4b134d6ca3007c
Boris <boris.wechselberger@youpers.com>
2014-02-11 14:54:43 +0100

    WL-89 - organization/campaign translation

--- app/yp.organization/yp.organization.js
+++ app/yp.organization/yp.organization.js
@@ -4,18 +4,17 @@
 
     angular.module('yp.organization', ['ui.router'])
 
-        .config(['$stateProvider', '$urlRouterProvider', 'accessLevels',
-            function ($stateProvider, $urlRouterProvider, accessLevels) {
+        .config(['$stateProvider', '$urlRouterProvider', 'accessLevels', '$translateWtiPartialLoaderProvider',
+            function ($stateProvider, $urlRouterProvider, accessLevels, $translateWtiPartialLoaderProvider) {
 
                 $stateProvider
                     .state('organization', {
                         url: '/organization',
                         templateUrl: 'yp.organization/yp.organization.create.html',
                         controller: 'CreateOrganizationController',
-                        access: accessLevels.user,
-                        resolve: {
-
-                        }
+                        access: accessLevels.user
                     });
+
+                $translateWtiPartialLoaderProvider.addPart('yp.organization');
             }]);
 }());
