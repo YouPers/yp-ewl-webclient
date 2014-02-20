db8946acfc7204ea97d45000db5357d7fc826137
Boris <boris.wechselberger@youpers.com>
2014-02-20 17:48:32 +0100

    WL-249 - input directive / account form

--- app/yp.commons/yp.commons.directive.ypinput.js
+++ app/yp.commons/yp.commons.directive.ypinput.js
@@ -31,19 +31,34 @@
                         throw 'ypInput: attribute "ypModel" is required in the parent form';
                     }
 
+                    // create model object in parent scope if it does not exist yet
+                    if(!$scope.$parent[$scope.ypModel]) {
+                        $scope.$parent[$scope.ypModel] = {};
+                    }
+
+                    // assign model object in this scope
+                    $scope[$scope.ypModel] = $scope.$parent[$scope.ypModel];
 
                     // load contents of the element to compile it, which was prevented by 'ng-non-bindable'
                     var contents = $element.contents();
 
-                    // include all attributes from parent input
+
                     var input = $element.find('input');
-                    _.forEach($attrs.$attr, function(key) {
-                        input.attr(key, $attrs[key] ? $attrs[key] : true);
-                    });
 
                     // modify ng-model, interpolation does not work here
                     input.attr('ng-model', $scope.ypModel + '.' + name);
 
+                    // include all attributes from parent input
+                    _.forEach($attrs.$attr, function(value, key, list) {
+                        input.attr($attrs.$attr[key], $attrs[key] ? $attrs[key] : true);
+                    });
+
+
+                    $scope.$watch($scope.ypFormName + '.' + name, function(input) {
+                        if(input) {
+                            $scope.errors = input.$error;
+                        }
+                    });
 
                     $element.children().removeAttr('ng-non-bindable');
                     $compile(contents)($scope);
