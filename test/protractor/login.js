7362d39da76dedddbd3882907253ae6f9f11c8bb
Boris <boris.wechselberger@youpers.com>
2014-03-26 15:36:55 +0100

    WL-322 - e2e test

--- test/protractor/login.js
+++ test/protractor/login.js
@@ -5,11 +5,18 @@ describe('GET home', function() {
     });
 
 
-    it('login and display the users fullname', function() {
+    it('logs in and displays the users fullname', function() {
 
         element(by.model('username')).sendKeys('test_ind1');
         element(by.model('password')).sendKeys('yp\n');
 
         expect(element(by.binding('principal.getUser().fullname')).getText()).toEqual('Test Individual 1');
+    });
+
+    it('does a quick and dirty assessment for later tests', function () {
+
+        browser.get('/#/assessment/525faf0ac558d40000000005');
+
+        element(by.css('.ass-done-button button')).click();
     });
 });
