3bcfea53fab843b429a9b4a891ec5f2727a103a7
RBLU <reto.blunschi@youpers.com>
2014-04-03 15:13:34 +0200

    add missing recWeights, because backend might not anymore preserve empty results

--- app/yp.activity/yp.activity.admin.js
+++ app/yp.activity/yp.activity.admin.js
@@ -4,7 +4,7 @@
 
     angular.module('yp.activity')
 
-        .controller('ActivityAdminCtrl', ['$scope', '$rootScope',  'activity', 'assessment', 'ActivityService', 'Restangular',
+        .controller('ActivityAdminCtrl', ['$scope', '$rootScope', 'activity', 'assessment', 'ActivityService', 'Restangular',
             function ($scope, $rootScope, activity, assessment, ActivityService, Restangular) {
 
                 if (!activity) {
@@ -24,22 +24,29 @@
                 $scope.assessment = assessment;
 
 
-                // Weihting to generate recommendation of activity based on answers of this assessment
+                // Weighting to generate recommendation of activity based on answers of this assessment
                 // initialize weights if they do not yet exist
                 if (!activity.recWeights || activity.recWeights.length === 0) {
                     activity.recWeights = [];
-                    _.forEach(assessment.questionCats, function (cat) {
-                        _.forEach(cat.questions, function (question) {
-                            activity.recWeights.push([question.id,0,0]);
-                        });
-                    });
                 }
+
+                // backend does not store emtpy weights, but our UI needs an empty record for each question
+                // so we add one for all question that don't have one
+                _.forEach(assessment.questionCats, function (cat) {
+                    _.forEach(cat.questions, function (question) {
+                        if (!_.any(activity.recWeights, function (recWeight) {
+                            return recWeight[0] === question.id;
+                        })) {
+                            activity.recWeights.push([question.id, 0, 0]);
+                        }
+                    });
+                });
 
                 $scope.recWeights = activity.getRecWeightsByQuestionId();
 
                 $scope.save = function () {
 
-                    ActivityService.saveActivity(activity).then(function(result) {
+                    ActivityService.saveActivity(activity).then(function (result) {
                         $rootScope.$emit('notification:success', 'activity.save');
                         $scope.$state.go('activitylist', $rootScope.$stateParams);
                     });
