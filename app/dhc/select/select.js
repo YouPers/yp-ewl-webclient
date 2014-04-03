4245a5b7df398092159fa42ea27410a371f12da2
ragnox <boris.wechselberger@gmail.com>
2014-04-03 17:11:12 +0200

    WL-348

--- app/dhc/select/select.js
+++ app/dhc/select/select.js
@@ -25,52 +25,19 @@
                             }
                         },
                         resolve: {
-
+                            offers: ['ActivityService', function(ActivityService) {
+                                return ActivityService.getActivityOffers();
+                            }]
                         }
                     });
 
                 $translateWtiPartialLoaderProvider.addPart('dhc/select/select');
             }])
 
-        .controller('SelectController', [ '$scope', '$rootScope', '$stateParams', '$location', '$window', '$timeout', 'ActivityService',
-            function ($scope, $rootScope, $stateParams, $location, $window, $timeout, ActivityService) {
+        .controller('SelectController', [ '$scope', '$rootScope', '$stateParams', '$location', '$window', '$timeout', 'offers',
+            function ($scope, $rootScope, $stateParams, $location, $window, $timeout, offers) {
 
-                // TODO: get width from DOM instead of hard coding it
-                var cardWidth = 20.4;
-
-                $scope.$watch('slider.itemsPerView', function () {
-                    $scope.outerListStyle = {
-                        width: cardWidth * $scope.slider.itemsPerView + 'em'
-                    };
-                });
-
-                $scope.$watch('slider.offset', function () {
-                    $location.search({ s: $scope.slider.offset });
-                    $scope.innerListStyle = {
-                        left: - $scope.slider.offset * cardWidth + 'em'
-                    };
-                });
-
-                $scope.slider = {
-                    offset: parseInt($stateParams.s) || 0,
-                    itemsPerView: 3,
-                    prev: function() {
-                        if($scope.slider.hasPrev()) {
-                            $scope.slider.offset -= 1;
-                        }
-                    },
-                    next: function() {
-                        if($scope.slider.hasNext()) {
-                            $scope.slider.offset += 1;
-                        }
-                    },
-                    hasPrev: function() {
-                        return $scope.slider.offset > 0;
-                    },
-                    hasNext: function() {
-                        return $scope.slider.items && $scope.slider.offset < $scope.slider.items.length - $scope.slider.itemsPerView;
-                    }
-                };
+                $scope.offers = offers;
 
                 // watch window width to adjust number of items displayed
                 $scope.$watch( function() {
@@ -92,26 +59,15 @@
                     } else {
                         n = 3;
                     }
-                    $scope.slider.itemsPerView = n;
+                    $scope.cardsPerView = n;
 
                 } );
 
-                $scope.isItemActive = function (index) {
-                    return $scope.slider.offset <= index && $scope.slider.offset + $scope.slider.itemsPerView > index;
-                };
 
-                $scope.showItem = function (index) {
-                    // place in the middle if 3 items are displayed
-                    $scope.slider.offset = index - ($scope.slider.itemsPerView === 3 ? 1 : 0);
-                };
-
-                ActivityService.getActivityOffers().then(function(offers) {
-                    $scope.slider.items = offers;
-                });
 
                 $scope.reject = function(index, event) {
                     event.stopPropagation();
-                    $scope.slider.items.splice(index, 1);
+                    $scope.offers.splice(index, 1);
                 };
 
 
