ed04187018bc80b16077fa3b49cb106c20a115da
ragnox <boris.wechselberger@gmail.com>
2014-04-15 11:46:38 +0200

    WL-384

--- app/dhc/welcome/welcome.js
+++ app/dhc/welcome/welcome.js
@@ -11,27 +11,47 @@
             function ($stateProvider, $urlRouterProvider, accessLevels, $translateWtiPartialLoaderProvider) {
                 $stateProvider
                     .state('welcome', {
+                        url: "/welcome/{campaignId}",
                         templateUrl: "dhc/welcome/welcome.html",
-                        access: accessLevels.all
-                    })
-                    .state('welcome.content', {
-                        url: "/welcome",
                         access: accessLevels.all,
                         resolve: {
 
+                            campaign: ['Restangular', '$stateParams',
+                                function (Restangular, $stateParams) {
+                                    return Restangular.one('campaigns', $stateParams.campaignId).get().then(function success (campaign) {
+                                        return campaign;
+                                    });
+                                }]
                         }
                     });
 
                 $translateWtiPartialLoaderProvider.addPart('dhc/welcome/welcome');
             }])
 
-        .controller('WelcomeController', [ '$scope', '$rootScope', 'CampaignService',
-            function ($scope, $rootScope, CampaignService) {
+        .controller('WelcomeController', [ '$scope', '$rootScope', '$state', '$stateParams', 'UserService', 'campaign',
+            function ($scope, $rootScope, $state, $stateParams, UserService, campaign) {
 
-                CampaignService.getCampaign('527916a82079aa8704000006').then(function(campaign) {
+                if (!campaign) {
+                    $state.go('home.content');
+                }
 
-                    $scope.campaign = campaign;
-                });
+                $scope.join = function () {
+
+                    UserService.principal.getUser().campaign = campaign.id;
+
+                    // if this is an authenticated user (e.g. a stored one, that already exists on the backend)
+                    // we need to save it, because we have just updated it.
+                    // if it is NOT an authenticated user we cannot save this change at this moment, so we
+                    // just leave it in the client principal and store it when the user eventually register on our site.
+                    if (UserService.principal.isAuthenticated()) {
+                        UserService.putUser(UserService.principal.getUser()).then(function (result){
+
+                            $state.go('home.content');
+
+                        });
+                    }
+
+                };
 
             }
         ]);
