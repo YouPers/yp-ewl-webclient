c5b8d07a3cd00bcb1bdeb50abd9e40b6bb9548e7
RBLU <reto.blunschi@youpers.com>
2014-04-25 07:43:55 +0200

    WL-461 WL-463

--- app/dhc/diary/diary.js
+++ app/dhc/diary/diary.js
@@ -24,7 +24,7 @@
                             }
                         },
                         resolve: {
-                            diaryEntries: ['DiaryService', function(DiaryService) {
+                            diaryEntries: ['DiaryService', function (DiaryService) {
                                 return DiaryService.getEntries({populate: 'activityPlan'});
                             }]
                         }
@@ -38,27 +38,30 @@
 
 
                 function _getEntryGroups(entries) {
-                    var groupedEntries = _.groupBy(_.sortBy(entries,function(entry) {return entry.dateBegin;}).reverse(), function(entry) {
+                    var groupedEntries = _.groupBy(
+                        _.sortBy(entries, function (entry) {
+                            return entry.dateBegin;
+                        }).reverse(), function (entry) {
 
-                        var date = entry.dateBegin;
+                            var date = entry.dateBegin;
 
-                        var diff = Math.abs(moment().hour(0).minute(0).second(0).millisecond(0).diff(date, 'days', true));
+                            var diff = Math.abs(moment().hour(0).minute(0).second(0).millisecond(0).diff(date, 'days', true));
 
-                        if ((diff > 1)) {
-                            return 'future';
-                        } else if(diff > 0) {
-                            return 'today';
-                        } else if(diff > -1) {
-                            return 'yesterday';
-                        } else if(diff > -7) {
-                            return 'week';
-                        } else if(diff > -31) {
-                            return 'month';
-                        } else {
-                            return 'other';
-                        }
+                            if ((diff > 1)) {
+                                return 'future';
+                            } else if (diff > 0) {
+                                return 'today';
+                            } else if (diff > -1) {
+                                return 'yesterday';
+                            } else if (diff > -7) {
+                                return 'week';
+                            } else if (diff > -31) {
+                                return 'month';
+                            } else {
+                                return 'other';
+                            }
 
-                    });
+                        });
 
                     var groupsNames = [
                         'today',
@@ -72,7 +75,7 @@
                     var groups = [];
 
                     _.forEach(groupsNames, function (group) {
-                        if(groupedEntries[group]) {
+                        if (groupedEntries[group]) {
                             groups.push({
                                 name: group,
                                 entries: groupedEntries[group]
@@ -86,38 +89,40 @@
                 $scope.groups = _getEntryGroups(diaryEntries);
 
 
-                $scope.showModal = function() {
-                    DiaryService.showDiaryModal().then(function(newEntry) {
-                        diaryEntries.push(newEntry);
-                        $scope.groups = _getEntryGroups(diaryEntries);
+                $scope.showModal = function () {
+                    DiaryService.showDiaryModal().then(function (newEntry) {
+                        if (newEntry) {
+                            diaryEntries.push(newEntry);
+                            $scope.groups = _getEntryGroups(diaryEntries);
+                        }
                     });
                 };
 
             }
         ])
 
-    .controller('DiaryModalController', ['$scope', '$modalInstance', 'topic', 'resolution',
-            function($scope, $modalInstance, topic, resolution) {
-            $scope.topic = topic;
-            $scope.resolution = resolution;
+        .controller('DiaryModalController', ['$scope', '$modalInstance', 'topic', 'resolution',
+            function ($scope, $modalInstance, topic, resolution) {
+                $scope.topic = topic;
+                $scope.resolution = resolution;
 
-            $scope.result = {
-                entry: {
-                    feedback: null,
-                    text: null,
-                    type: 'manual',
-                    dateBegin: new Date()
-                },
-                doNotAskAgainToday: false
-            };
+                $scope.result = {
+                    entry: {
+                        feedback: null,
+                        text: null,
+                        type: 'manual',
+                        dateBegin: new Date()
+                    },
+                    doNotAskAgainToday: false
+                };
 
-            $scope.ok = function () {
-                $modalInstance.close($scope.result);
-            };
+                $scope.ok = function () {
+                    $modalInstance.close($scope.result);
+                };
 
-            $scope.cancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
-        }]);
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            }]);
 
 }());
