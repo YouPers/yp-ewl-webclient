09525bc9981d9da265da4044f48c5c1a1b2d5e6c
ragnox <boris.wechselberger@gmail.com>
2014-04-29 14:02:57 +0200

    WL-543

--- Gruntfile.js
+++ Gruntfile.js
@@ -235,7 +235,7 @@ module.exports = function (grunt) {
                     {
                         expand: true,
                         cwd: '<%= yeoman.app %>/assets',
-                        src: '{,*/}*.{png,jpg,jpeg}',
+                        src: '**/{,*/}*.{png,jpg,jpeg}',
                         dest: '<%= yeoman.dist %>/assets'
                     }
                 ]
