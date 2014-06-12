5dc3a6b4a864ced84d3336b287afc52ff6ba9abd
RBLU <reto.blunschi@youpers.com>
2014-06-12 17:12:54 +0200

    added possibility to dismiss coachmessage

--- app/components/health-coach/health-coach-directive.js
+++ app/components/health-coach/health-coach-directive.js
@@ -56,6 +56,9 @@
                         }
                     };
 
+                    scope.dismiss = function() {
+                        scope.coachMessages = [];
+                    };
 
                     $rootScope.$on('healthCoach:displayMessage', function (event, message, interpolateParams) {
                         if (!scope.coachMessages) {
