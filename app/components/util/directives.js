7e3dce58047ce243ab45bb39324ac6e061d763e8
ragnox <boris.wechselberger@gmail.com>
2014-06-20 20:04:50 +0200

    WL-496

--- app/components/util/directives.js
+++ app/components/util/directives.js
@@ -4,20 +4,6 @@
 
     angular.module('yp.components.util.directives', [])
 
-        .directive('ngEnter', function () {
-            return function (scope, element, attrs) {
-                element.bind("keydown keypress", function (event) {
-                    if(event.which === 13) {
-                        scope.$apply(function (){
-                            scope.$eval(attrs.ngEnter);
-                        });
-
-                        event.preventDefault();
-                    }
-                });
-            };
-        })
-
         .directive('form', [function () {
             return {
                 restrict: 'E',
