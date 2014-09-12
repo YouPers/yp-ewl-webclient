4d02bd31ba70eb8667ba74f337d1511f23233e33
RBLU <reto.blunschi@youpers.com>
2014-09-12 10:15:27 +0200

    WL-1014

--- app/dcm/home/home.js
+++ app/dcm/home/home.js
@@ -18,14 +18,18 @@
                         },
                         resolve: {
 
-                            socialInteractions: ['$stateParams', 'SocialInteractionService', function($stateParams, SocialInteractionService) {
+                            socialInteractions: ['$stateParams', 'SocialInteractionService', function ($stateParams, SocialInteractionService) {
                                 return SocialInteractionService.getSocialInteractions({
                                     populate: 'author',
                                     targetId: $stateParams.campaignId,
                                     authored: true,
                                     authorType: 'campaignLead'
                                 });
+                            }],
+                            jsInclude: ["util", function (util) {
+                                return util.loadJSInclude('lib/d3/d3.js');
                             }]
+
 
                         }
                     });
@@ -34,16 +38,16 @@
             }])
 
 
-
         .controller('HomeController', ['$scope', '$rootScope', '$state', 'UserService', 'socialInteractions', 'campaign', 'campaigns',
             function ($scope, $rootScope, $state, UserService, socialInteractions, campaign, campaigns) {
 
+                $scope.campaign = campaign;
 
                 if (!campaign && campaigns.length > 0) {
                     $state.go('dcm.home', { campaignId: campaigns[0].id });
                 }
 
-                $scope.offers = _.filter(socialInteractions, function(si) {
+                $scope.offers = _.filter(socialInteractions, function (si) {
                     return si.__t === 'Recommendation' || si.__t === 'Invitation';
                 });
                 _.each($scope.offers, function (offer) {
@@ -52,5 +56,19 @@
 
                 $scope.isOrganizationAdmin = _.contains(UserService.principal.getUser().roles, 'orgadmin');
 
-            }]);
+            }])
+
+        .controller('HomeStatsController', ['$scope', 'StatsService', function ($scope, StatsService) {
+            $scope.chartData = {};
+            if ($scope.campaign) {
+                StatsService.loadStats($scope.campaign.id, {type: 'newUsersPerDay', scopeType: 'campaign', scopeId: $scope.campaign.id}).then(function (result) {
+                    $scope.chartData.newUsers = StatsService.fillAndFormatForPlot(result[0].newUsersPerDay, $scope.campaign, ['count'], true);
+                });
+
+                StatsService.loadStats($scope.campaign.id, {type: 'activitiesPlannedPerDay', scopeType: 'Campaign', scopeId: $scope.campaign.id}).then(function (result) {
+                    $scope.chartData.plannedActs = StatsService.fillAndFormatForPlot(result[0].activitiesPlannedPerDay, $scope.campaign);
+                });
+            }
+
+        }]);
 }());
