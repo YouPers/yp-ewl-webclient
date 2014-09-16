e002f8ef1eaed3328a2fa4f0f43cf1310829b506
RBLU <reto.blunschi@youpers.com>
2014-09-16 08:29:39 +0200

    WL-888 home dispatcher

--- app/yp.ewl/yp.ewl.js
+++ app/yp.ewl/yp.ewl.js
@@ -26,10 +26,22 @@ angular.module('yp-ewl',
         function ($stateProvider, $urlRouterProvider, accessLevels, RestangularProvider, config, $translateProvider, $translateWtiPartialLoaderProvider) {
             //
             // For any unmatched url, send to /home
-            $urlRouterProvider.otherwise('/campaign//game/');
+            $urlRouterProvider.otherwise('/dispatch');
             //
             // Now set up the states
             $stateProvider
+                .state('homedispatcher', {
+                    url: "/dispatch",
+                    access: accessLevels.all,
+                    controller: ['UserService', '$state', function (UserService, $state) {
+                        if (UserService.principal.isAuthorized(accessLevels.campaignlead)) {
+                            return $state.go('dcm.home');
+                        } else {
+                            return $state.go('dhc.game');
+                        }
+                    }]
+                })
+
                 .state('terms', {
                     url: "/terms",
                     templateUrl: "partials/terms.html",
