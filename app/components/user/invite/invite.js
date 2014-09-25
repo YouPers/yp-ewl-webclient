684c92a263ba250158205b9ecc481f0ad71fe7d5
ragnox <boris.wechselberger@gmail.com>
2014-09-25 10:57:29 +0200

    WL-1076

--- app/components/user/invite/invite.js
+++ app/components/user/invite/invite.js
@@ -26,38 +26,6 @@
 
                             campaign: ['CampaignService', 'invitation', function (CampaignService, invitation) {
                                 return CampaignService.getCampaign(invitation.activity.campaign);
-                            }],
-
-                            onSignIn: ['$state', '$stateParams', 'UserService', 'invitation', function ($state, $stateParams, UserService, invitation) {
-                                var onSignIn = function() {
-
-                                    // check the users campaign against the campaign of the activity
-                                    // log him out if it does not match, and show a message
-
-                                    var inviteController = this.inviteController;
-                                    var user = UserService.principal.getUser();
-                                    if(invitation.activity.campaign !== user.campaign.id) {
-                                        UserService.logout().then(function () {
-                                            inviteController.invalidCampaignUser = user;
-                                        });
-                                    } else {
-                                        $state.go('dhc.activity' , {
-                                            campaignId: invitation.activity.campaign, //TODO: check if it is the same as the users campaign
-                                            idea: invitation.activity.idea.id,
-                                            activity: invitation.activity.id,
-                                            socialInteraction: invitation.id
-                                        });
-                                    }
-                                };
-
-
-                                // if the user is authenticated we immediatly go to the corresponding activity so he can join
-                                if (UserService.principal.isAuthenticated()) {
-                                    onSignIn();
-                                } else {
-                                    return onSignIn;
-                                }
-
                             }]
                         }
                     });
@@ -65,9 +33,41 @@
                 $translateWtiPartialLoaderProvider.addPart('components/user/invite/invite');
             }])
 
-        .controller('InviteController', [ '$scope', '$rootScope', '$state', '$stateParams', 'UserService', 'invitation', 'campaign', 'onSignIn',
-            function ($scope, $rootScope, $state, $stateParams, UserService, invitation, campaign, onSignIn) {
+        .controller('InviteController', [ '$scope', '$rootScope', '$state', '$stateParams', 'UserService', 'invitation', 'campaign',
+            function ($scope, $rootScope, $state, $stateParams, UserService, invitation, campaign) {
 
+                var inviteController = this;
+
+                var onSignIn = function() {
+
+                    // check the users campaign against the campaign of the activity
+                    // log him out if it does not match, and show a message
+
+                    var user = UserService.principal.getUser();
+                    if(!user.campaign) {
+                        $rootScope.$emit('clientmsg:error', 'userWithoutcampaign');
+                        return;
+                    }
+
+                    if(invitation.activity.campaign !== user.campaign.id) {
+                        UserService.logout().then(function () {
+                            inviteController.invalidCampaignUser = user;
+                        });
+                    } else {
+                        $state.transitionTo('dhc.activity' , {
+                            campaignId: invitation.activity.campaign, //TODO: check if it is the same as the users campaign
+                            idea: invitation.activity.idea.id,
+                            activity: invitation.activity.id,
+                            socialInteraction: invitation.id
+                        });
+                    }
+                };
+
+
+                // if the user is authenticated we immediatly go to the corresponding activity so he can join
+                if (UserService.principal.isAuthenticated()) {
+                    onSignIn();
+                }
 
                 $scope.onSignIn = onSignIn;
 
