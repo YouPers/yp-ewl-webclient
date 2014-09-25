9acc4df688ec3cb572d99167d20d8925bc5ed98f
RBLU <reto.blunschi@youpers.com>
2014-09-25 09:08:30 +0200

    cleanup config, add config for localvm windows tests

--- config/config.js
+++ config/config.js
@@ -1,56 +1,65 @@
 /**
  *
- * translationSource: source for the translation resources, values: 'local', 'wti'
+ * environment config, used by grunt to produce an environment specific build based on the node env
+ *
+ * IMPORTANT: This file is not packaged and sent to the client, BUT the grunt task produces a
+ * environment-specific index.html based on the NODE_ENV variable that grunt finds when executing 'grunt build'.
+ * Grunt calls "environmentConfig[process.env.NODE_ENV || 'dev'] to get the correct config object.
+ *
+ * explanations for supported config parameters:
+ *
+ * - translationSource: source for the translation resources, values: 'local', 'wti'
  *
  *
  *
  */
+var _ = require('lodash');
 
+var defaultConfig = {
+    name: 'default',
+    webclientUrl: 'unspecified: adjust-config.js',
+    backendUrl: 'unspecified: adjust-config.js',
+    translationSource: 'local',
+    googleAnalytics: {
+        webPropertyId: 'UA-39309635-4'
+    },
+    ui: {
+        showUserFeedbackLink: true
+    }
+};
 
-var environmentConfig = {
+var specificConfigs = {
     dev: {
         name: 'dev',
-        webclientUrl: 'http://dev:9000',
-        backendUrl: 'http://dev:8000',
-        translationSource: 'local',
-        googleAnalytics: {
-            webPropertyId: 'UA-39309635-4'
-        },
-        ui: {
-            showUserFeedbackLink: true
-        }
+        webclientUrl: 'http://localhost:9000',
+        backendUrl: 'http://localhost:8000'
     },
-    test: {
-        name: 'test',
-        webclientUrl: 'http://dev:9000',
-        backendUrl: 'http://dev:8000',
-        translationSource: 'local',
-        googleAnalytics: {
-            webPropertyId: 'UA-39309635-4'
-        },
-        ui: {
-            showUserFeedbackLink: true
-        }
+    localvm: {
+        // used to test from Windows VMs on your local machine,
+        // REQUIRES: - a /etc/hosts entry for 'localvm' to the real IP of you mac
+        //           - set the NODE_ENV to 'localvm' in the shell where your weblclient is grunted.
+        name: 'localvm',
+        webclientUrl: 'http://localvm:9000',
+        backendUrl: 'http://localvm:8000'
     },
-    ci: {
+
+// RBLU: I think this is not used anywhere, so commenting out and checking in to see what happens
+//    test: {
+//        name: 'test',
+//        webclientUrl: 'http://localhost:9000',
+//        backendUrl: 'http://localhost:8000'
+//    },
+    ci: { // used by circleci shell when building no the circleci vm
         name: 'ci',
         webclientUrl: 'http://localhost:9000',
         backendUrl: 'https://cimaster.youpers.com/api',
-        translationSource: 'local',
-        googleAnalytics: {},
-        ui: {
-            showUserFeedbackLink: true
-        }
+        googleAnalytics: {}
     },
-    cimaster: {
+    cimaster: { // used by automtatic deploys on cimaster machine
         name: 'cimaster',
         webclientUrl: 'https://cimaster.youpers.com',
         backendUrl: 'https://cimaster.youpers.com/api',
-        translationSource: 'local',
-        googleAnalytics: {},
-        ui: {
-            showUserFeedbackLink: true
-        }
+        googleAnalytics: {}
     },
     uat: {
         name: 'uat',
@@ -58,28 +67,18 @@ var environmentConfig = {
         translationSource: 'local',
         googleAnalytics: {
             webPropertyId: 'UA-39309635-2'
-        },
-        ui: {
-            showUserFeedbackLink: true
         }
-    },
-    nb: {
-        backendUrl: 'https://nb.youpers.com/api',
-        translationSource: 'wti',
-        googleAnalytics: {}
     },
     prod: {
         backendUrl: 'https://prod.youpers.com/api',
-        translationSource: 'local',
         googleAnalytics: {
             webPropertyId: 'UA-39309635-3'
-        },
-        ui: {
-            showUserFeedbackLink: true
         }
     }
 };
 
 module.exports = {
-    environmentConfig: environmentConfig
+    environmentConfig: _.forEach(specificConfigs, function (conf, key) {
+        _.defaults(conf, defaultConfig);
+    })
 };
