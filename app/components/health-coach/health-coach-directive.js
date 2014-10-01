1513dd37a0bde6c148cd106bd9df2d315caa184f
Boris <boris.wechselberger@youpers.com>
2014-10-01 14:32:08 +0200

    WL-1127

--- app/components/health-coach/health-coach-directive.js
+++ app/components/health-coach/health-coach-directive.js
@@ -2,6 +2,21 @@
 
     'use strict';
 
+    /**
+     * directive health-coach
+     *
+     * there 4 ways to display a health coach message:
+     *
+     *  - scope attribute 'event' - the event name will be localized according to the state: [dhc,dcm].stateName.eventName
+     *  - $rootScope.$emit('healthCoach:event', eventName) - will be localized as above
+     *  - $rootScope.$emit('healthCoach:displayMessage') - display an already localized message
+     *  - queue an event for the next state using HealthCoachService.queueEvent,
+     *    it will be consumed and cleared on the next invocation of the health-coach directive
+     *
+     *
+     *
+     */
+
     angular.module('yp.components.healthCoach')
         .directive('healthCoach', ['$rootScope', 'HealthCoachService', '$window', '$timeout', '$state', '$translate','$sce',
             function ($rootScope, HealthCoachService, $window, $timeout, $state, $translate, $sce) {
