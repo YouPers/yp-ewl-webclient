5eb9f61d1f30dcc6da22ea6926ea7a8e9783375f
RBLU <reto.blunschi@youpers.com>
2014-12-02 11:52:02 +0100

    fix for redirecting loops

--- app/dhc/dhc.js
+++ app/dhc/dhc.js
@@ -38,26 +38,7 @@
                         }]
                     },
 
-                    onEnter: ['$rootScope', '$state', 'UserService', function($rootScope, $state, UserService){
-                        var user = UserService.principal.getUser();
-                        var campaign = user.campaign;
-
-                        // last 2 days of the users campaign -> redirect non-campaignAdmins to dhc end of campaign
-                        // if endOfCampaignDisplayed has not been set yet, or it is more than 1 day in the past
-                        if(campaign && !$rootScope.isCampaignAdmin &&
-                            (!campaign.endOfCampaignDisplayed || moment().diff(campaign.endOfCampaignDisplayed, 'days') > 1) &&
-                            moment().diff(campaign.end, 'days') >= -2) {
-                            campaign.endOfCampaignDisplayed = moment();
-                            return $state.transitionTo('dhc.end-of-campaign', { campaignId: campaign.id });
-                        }
-
-                        if (!campaign) {
-                            if(user.campaign){
-                                $state.go('dhc.game', { campaignId: user.campaign.id || user.campaign });
-                            } else {
-                                $state.go('campaign-list.content');
-                            }
-                        }
+                    onEnter: ['$rootScope', '$state', '$stateParams', 'campaign', 'UserService', function($rootScope, $state, $stateParams, campaign, UserService){
                     }]
                 });
 
@@ -66,9 +47,32 @@
 
         .controller('DhcController', ['$scope', '$rootScope', '$state', '$timeout', 'UserService', 'campaign',
             function ($scope, $rootScope, $state, $timeout, UserService, campaign) {
+                var user = UserService.principal.getUser();
+                console.log('DhcController is run now.');
 
-                $scope.parentState = 'dhc';
-                $scope.currentCampaign = campaign;
+                if (!campaign) {
+                    if(user.campaign){
+//                                $state.go('dhc.game', { campaignId: user.campaign.id || user.campaign , view: ""});
+                        console.log('DhcController: redirecting to dhc.game (no campaign in URL');
+                        $state.go('dhc.game', { campaignId: user.campaign.id || user.campaign , view: ""});
+
+                    } else {
+                        console.log('DhcController: redirecting to campaign list(no campaign in URL, no campaign on user');
+                        $state.go('campaign-list.content');
+                    }
+                } else {
+                    // last 2 days of the users campaign -> redirect non-campaignAdmins to dhc end of campaign
+                    // if endOfCampaignDisplayed has not been set yet, or it is more than 1 day in the past
+                    if(!$rootScope.isCampaignAdmin &&
+                        (!campaign.endOfCampaignDisplayed || moment().diff(campaign.endOfCampaignDisplayed, 'days') > 1) &&
+                        moment().diff(campaign.end, 'days') >= -2) {
+                        campaign.endOfCampaignDisplayed = moment();
+                        // return $state.go('dhc.end-of-campaign', { campaignId: campaign.id });
+                        console.log('DhcController: redirecting end of campaign');
+                        return $state.go('dhc.end-of-campaign', { campaignId: campaign.id });
+                    }
+
+                }
             }]);
 
 }());
