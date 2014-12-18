90caeda078c85c04d5d67574f2108116227f958d
RBLU <reto.blunschi@youpers.com>
2014-12-18 12:44:21 +0100

    add a maintenance.html page

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+touch /home/youpers/maintenance
 cd /home/youpers/yp-ewl-webclient
 git pull origin $1
 export NODE_ENV=$2
 npm install
 grunt build
 sudo /usr/sbin/service nginx  restart
+rm /home/youpers/maintenance
