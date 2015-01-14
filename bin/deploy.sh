7d98e7d6e249511ad3a6499e1ac937eaa71b360f
RBLU <reto.blunschi@youpers.com>
2015-01-14 21:50:04 +0100

    missing translation text

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
-cd /home/youpers/yp-ewl-webclient
+cd /home/youpers/$2-frontend
 git pull origin $1
 export NODE_ENV=$2
 npm install  # actually npm upadte would be better but takes 10 minutes
 
 # enable maintenance mode: our nginx config looks for this file
-touch /home/youpers/maintenance
+touch /home/youpers/maintenance-$2
 
 grunt build
 sudo /usr/sbin/service nginx  restart
 
 # disable maintenance mode again
-rm /home/youpers/maintenance
+rm /home/youpers/maintenance-$2
