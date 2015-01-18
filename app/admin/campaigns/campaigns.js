cb876975d9a2f6255372009e3d851100fc44002c
Boris <boris.wechselberger@youpers.com>
2015-01-18 16:47:44 +0100

    WL-1470 - admin campaign list

---
+++ app/admin/campaigns/campaigns.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+
+    angular.module('yp.admin')
+
+
+        .config(['$stateProvider', '$urlRouterProvider', 'accessLevels',
+            function ($stateProvider, $urlRouterProvider, accessLevels) {
+                $stateProvider
+                    .state('admin-campaigns', {
+                        templateUrl: "layout/admin-default.html",
+                        access: accessLevels.admin
+                    })
+
+                    .state('admin-campaigns.content', {
+                        url: "/admin/campaigns",
+                        access: accessLevels.admin,
+                        views: {
+                            content: {
+                                templateUrl: "admin/campaigns/campaigns.html",
+                                controller: 'AdminCampaignsCtrl'
+                            }
+                        },
+                        resolve: {
+                            campaigns: ['CampaignService', function(CampaignService) {
+                                return CampaignService.getCampaigns({ populate: 'organization campaignLeads' });
+                            }]
+                        }
+                    });
+            }])
+
+        .controller('AdminCampaignsCtrl', [
+            '$scope', '$state', 'campaigns',
+            function ($scope, $state, campaigns) {
+
+                $scope.campaigns = campaigns;
+                $scope.open = function (campaign) {
+                    $state.go('dcm.home', { campaignId: campaign.id});
+                };
+
+        }]);
+
+
+}());
