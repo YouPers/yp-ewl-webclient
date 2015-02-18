1f930c54c195b0db6f080860239fdb6745c407be
Boris <boris.wechselberger@youpers.com>
2015-02-18 13:39:15 +0100

    WL-1661

--- app/dcm/home/home.js
+++ app/dcm/home/home.js
@@ -291,6 +291,7 @@
 
                     StatsService.loadStats($scope.campaign.id, {type: 'newUsersPerDay', scopeType: 'campaign', scopeId: $scope.campaign.id}).then(function (result) {
                         var options = {
+                            runningTotal: true,
                             newestDay: moment.min(moment(), moment($scope.campaign.end)),
                             oldestDay: moment($scope.campaign.start),
                             legend: [
@@ -353,6 +354,228 @@
                 }
 
             }
-        }]);
+        }])
+
+
+        .controller('DcmEndOfCampaignController', [ '$scope', '$q', '$translate', '$rootScope', 'UserService', 'StatsService', 'ActivityService', 'AssessmentService',
+            function ($scope, $q, $translate, $rootScope, UserService, StatsService, ActivityService, AssessmentService) {
+
+                if (!$scope.campaign) {
+                    return;
+                }
+                $scope.daysLeft = - moment().diff($scope.campaign.end, 'days');
+                $scope.campaignEnded = moment().diff($scope.campaign.end) > 0;
+
+                $scope.percetageFn = function (value) {
+                    return Math.round(value *100) + '%';
+                };
+                $scope.roundFn = function (value) {
+                    return Math.round(value);
+                };
+                $scope.onlyRoundFn = function (value) {
+                    return Math.round(value) === value ? value : '';
+                };
+                $scope.round1Fn = function (value) {
+                    return Math.round(value * 10) / 10;
+                };
+
+
+                init();
+
+                function init() {
+
+
+                    StatsService.loadStats($scope.campaign.id,
+                        {
+                            type: 'usersTotal',
+                            scopeType: 'campaign',
+                            scopeId: $scope.campaign.id
+                        }).then(function (results) {
+                            var res = results[0].usersTotal;
+
+                            $scope.campaignParticipants = [
+                                {
+                                    "key": $translate.instant('dcm-end-of-campaign.usersTotal.title'),
+                                    "values": [ [ $translate.instant('dcm-end-of-campaign.usersTotal.title') , res.usersTotal]  ]
+                                },
+                                {
+                                    "key": $translate.instant('dcm-end-of-campaign.usersTotal.title'),
+                                    "values": [ [ $translate.instant('dcm-end-of-campaign.usersTotal.title') , res.usersAvg]  ]
+                                }
+                            ];
+
+                        });
+
+
+
+                    function findByStatus(results, type, status) {
+                        var res = results[0][type];
+                        return (_.find(res, { status: status}) || {}).count;
+                    }
+                    // eventsStatus / eventsStatusAvg
+                    var eventStatus = [];
+                    $q.all([
+
+
+                        StatsService.loadStats($scope.campaign.id,
+                            {
+                                type: 'eventsStatusAvg',
+                                scopeType: 'campaign',
+                                scopeId: $scope.campaign.id
+                            }).then(function (results) {
+                                var type = 'eventsStatusAvg';
+                                eventStatus.push({
+                                    "key": $translate.instant('dcm-end-of-campaign.eventsStatus.campaign'),
+                                    "values": [
+                                        [$translate.instant('dcm-end-of-campaign.eventsStatus.done'), findByStatus(results, type, 'done')],
+                                        [$translate.instant('dcm-end-of-campaign.eventsStatus.missed'), findByStatus(results, type, 'missed')],
+                                        [$translate.instant('dcm-end-of-campaign.eventsStatus.open'), findByStatus(results, type, 'open')]]
+                                });
+
+                            }),
+                        StatsService.loadStats($scope.campaign.id,
+                            {
+                                type: 'eventsStatusAvg'
+                            }).then(function (results) {
+                                var type = 'eventsStatusAvg';
+                                eventStatus.push({
+                                    "key": $translate.instant('dcm-end-of-campaign.eventsStatus.average'),
+                                    "values": [
+                                        [$translate.instant('dcm-end-of-campaign.eventsStatus.done'), findByStatus(results, type, 'done')],
+                                        [$translate.instant('dcm-end-of-campaign.eventsStatus.missed'), findByStatus(results, type, 'missed')],
+                                        [$translate.instant('dcm-end-of-campaign.eventsStatus.open'), findByStatus(results, type, 'open')]]
+                                });
+
+                            })
+
+                    ]).then(function () {
+                        $scope.eventStatus = eventStatus;
+                    });
+
+                    $q.all([
+                        StatsService.getRatingsStats('campaign', $scope.campaign.id),
+                        StatsService.getRatingsStats('all', $scope.campaign.id)
+                    ]).then(function (results) {
+                        $scope.eventsRatings = results;
+                    });
+
+
+                    // assessmentResults
+
+                    $scope.displayInfo = function(question) {
+                        $rootScope.$emit('healthCoach:displayMessage', AssessmentService.renderCoachMessageFromQuestion(question));
+                    };
+
+                    StatsService.loadStats($scope.campaign.id,
+                        {
+                            type: 'assessmentResults',
+                            scopeType: 'campaign',
+                            scopeId: $scope.campaign.id,
+                            dontReplaceIds: 'true'
+                        }).then(function (results) {
+                            var type = 'assessmentResults';
+                            var res = results[0][type];
+
+                            $scope.assessmentResults = res;
+
+                            $q.all(
+                                [
+                                    StatsService.loadStats($scope.campaign.id,
+                                        {
+                                            type: 'assessmentResults',
+                                            scopeType: 'topic',
+                                            scopeId: $scope.campaign.topic.id,
+                                            dontReplaceIds: 'true'
+                                        }).then(function (results) {
+                                            var type = 'assessmentResults';
+                                            var res = results[0][type];
+
+                                            $scope.assessmentResultsAverage = res;
+
+                                        }),
+                                    AssessmentService.getAssessment($scope.campaign.topic.id || $scope.campaign.topic).then(function (assessment) {
+
+                                        $scope.categories = _.groupBy(assessment.questions, 'category');
+                                        $scope.orderedCategoryNames = _.uniq(_.map(assessment.questions, 'category'));
+                                        $scope.assessment = assessment;
+                                    })
+                                ])
+
+                                .then(function () {
+
+                                    $scope.assessmentResultStyle = function (val) {
+                                        return { flex: '0 0 ' + val * 100 + '%' };
+                                    };
+
+                                    _.each($scope.assessmentResults.concat($scope.assessmentResultsAverage), function (result, index) {
+                                        result.sum = result.veryNeg + result.neg + result.zero + result.pos + result.veryPos;
+                                        var percentages = [
+                                            result.veryNeg / result.sum,
+                                            result.neg / result.sum,
+                                            result.zero / result.sum,
+                                            result.pos / result.sum,
+                                            result.veryPos / result.sum
+                                        ];
+                                        result.percentages = [];
+                                        _.each(percentages, function (val, index) {
+                                            result.percentages.push(val);
+                                        });
+                                    });
+
+                                    // map assessment questions to stats questions
+                                    $scope.assessmentResultsByQuestion = {};
+                                    _.each($scope.assessmentResults, function (assessmentResult, index) {
+                                        $scope.assessmentResultsByQuestion[assessmentResult.question] = assessmentResult;
+                                    });
+                                    $scope.assessmentResultsAverageByQuestion = {};
+                                    _.each($scope.assessmentResultsAverage, function (assessmentResult, index) {
+                                        $scope.assessmentResultsAverageByQuestion[assessmentResult.question] = assessmentResult;
+                                    });
+                                });
+
+
+                        });
+
+                    $scope.formatValue = function (val, $index, question) {
+                        return val === 1 ? ($index === 2 ? question.midtext : ($index < 2 ? question.mintext : question.maxtext)) : (Math.round(val * 100) + '%');
+                    };
+
+                    $scope.isCategoryEmpty = function (questions) {
+                        return !_.any(questions, function (question) {
+                            return $scope.assessmentResultsByQuestion[question.id];
+                        });
+                    };
+
+
+                    // eventsPlanned, popular activities
+
+
+                    StatsService.loadStats($scope.campaign.id,
+                        {
+                            type: 'eventsPlanned',
+                            scopeType: 'campaign',
+                            scopeId: $scope.campaign.id,
+                            dontReplaceIds: 'true'
+                        })
+                        .then(function (results) {
+                            var type = 'eventsPlanned';
+                            var res = results[0][type];
+                            _.each(res, function (event) {
+                                event.idea = event._id;
+                            });
+                            return res;
+                        })
+                        .then(ActivityService.populateIdeas)
+                        .then(function (res) {
+                            $scope.eventsPlanned = res;
+                        });
+
+                }
+
+
+
+
+            }
+        ]);
 
 }());
