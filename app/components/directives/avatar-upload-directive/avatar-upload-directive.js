0fcdccc4cb09bea06a6a92477bfe23d2868a75da
Boris <boris.wechselberger@youpers.com>
2015-04-23 14:42:08 +0200

    WL-1948 - avatar directive refacoring

--- app/components/directives/avatar-upload-directive/avatar-upload-directive.js
+++ app/components/directives/avatar-upload-directive/avatar-upload-directive.js
@@ -11,15 +11,7 @@
     /**
      * directive: avatar-upload
      *
-     * dependencies: avatarObject in parent scope, with the avatar at avatarObject.avatar
-     *               or a custom object specified as attribute
-     *
-     * default: user avatar for the authenticated user
-     *
-     * optional attributes:
-     *
-     *  - type: [ 'organization' ]
-     *  - avatarObject: the object where the avatar is located, avatarObject.avatar
+     * user avatar upload directive for the authenticated user
      *
      */
         .directive('avatarUpload', ['UserService', 'ImageService',
@@ -28,47 +20,42 @@
                     restrict: 'E',
                     transclude: true,
                     templateUrl: 'components/directives/avatar-upload-directive/avatar-upload-directive.html',
-                    scope: {
-                        avatarObject: "="
-                    },
+                    scope: { },
                     priority: 10,
                     link: function (scope, elem, attrs) {
 
-                        if (!scope.avatarObject && (!attrs.avatarObject || !scope[attrs.avatarObject])) {
-                            throw "avatar-upload: avatarObject not found or specified";
-                        } else {
-
-                            var avatarObject = scope.avatarObject;
-
-                            if (!avatarObject || _.isEmpty(avatarObject)) {
-                                return;
-                            }
-
-                            scope.avatarObject = avatarObject;
-
-                            scope.uploader = ImageService.getImageUploader(attrs.type || 'user', scope,
-                                function successCb(url) {
-                                    var user = UserService.principal.getUser();
-                                    user.avatar = url;
-                                    UserService.putUser(user).then(function(savedUser) {
-                                        scope.avatarObject = savedUser;
-                                        scope.$root.$emit('clientmsg:success','pictureSaved');
-                                    });
+                        scope.user = UserService.principal.getUser();
+                        scope.uploader = ImageService.getImageUploader(attrs.type || 'user', scope,
+                            function successCb(url) {
+                                var user = UserService.principal.getUser();
+                                user.avatar = url;
+                                UserService.putUser(user).then(function(savedUser) {
+                                    scope.user = savedUser;
+                                    scope.$root.$emit('clientmsg:success','pictureSaved');
                                 });
-                        }
+                            });
                     }
                 };
             }])
 
 
-        .directive('avatar', function () {
-            return function (scope, element, attrs) {
+        .directive('avatar', ['UserService', function (UserService) {
+            return {
+                restrict: 'E',
+                template: '<img ng-if="!showAvatarUpload" class="avatar" ng-src="{{user.avatar}}"><avatar-upload ng-if="showAvatarUpload"></avatar-upload>',
+                scope: {
+                    user: '='
+                },
+                link: function (scope, elem, attrs) {
 
-                scope.$watch('avatarObject.avatar', function (avatar) {
-                    if (avatar) {
-                        element.css("background-image", "url(" + avatar + ")");
+                    if(!scope.user) {
+                        scope.user = UserService.principal.getUser();
+                        if(UserService.hasDefaultAvatar()) {
+                            scope.showAvatarUpload = true;
+                        }
                     }
-                });
+
+                }
             };
-        });
+        }]);
 }());
